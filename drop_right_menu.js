532d83cf752639f12805bcdf63d7d7d3e8cac294
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2023-08-25 23:09:34 +0700

    pppp

--- drop_right_menu.js
+++ drop_right_menu.js
@@ -1,13 +1,4 @@
-const mainContainer = document.getElementById('main-container');
-const dropRightMenu = document.getElementById('drop-right-menu');
-const logo = document.querySelector('.logo');
 
-// open drop right menu
-mainContainer.addEventListener('mousemove', function () {
-    dropRightMenu.style.display = 'block';
-})
 
-// close drop right menu
-mainContainer.addEventListener('mouseout', function () {
-    dropRightMenu.style.display = 'none';
-})
+
+
