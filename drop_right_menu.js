b2bebef513286082de523b8a8193aa13f0fee519
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2023-08-26 15:10:51 +0700

    version 0.0.0.1

--- drop_right_menu.js
+++ drop_right_menu.js
@@ -1,4 +1,22 @@
+const mainContainer = document.getElementById('main-container');
+// const dropRightMenu = document.getElementById('drop-right-menu');
+mainContainer.addEventListener('mousemove', () => {
+   mainContainer.style.width = '320px';
+   mainContainer.style.transition = 'width 0.3s ease';
+});
 
+mainContainer.addEventListener('mouseout', () => {
+   mainContainer.style.width = '50px';
+});
 
+const container = document.getElementById('container');
+container.addEventListener('mousemove', () => {
+   container.style.backgroundColor = '#2a2a2e';
+   container.style.transition = 'width 0.3s ease';
+   container.style.width = 'auto';
+});
 
-
+container.addEventListener('mouseout', () => {
+   container.style.backgroundColor = '#121216';
+   container.style.transition = 'width 0.3s ease';
+});
