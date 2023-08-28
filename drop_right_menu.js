e1fe2d65bbd616732f763e8a9aeb5bbd6ddc0a2c
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2023-08-28 14:56:06 +0700

    version 0.0.0.4

--- drop_right_menu.js
+++ drop_right_menu.js
@@ -1,21 +1,10 @@
-// style main container
-const mainContainer = document.getElementById('main-container');
-mainContainer.addEventListener('mousemove', () => {
-   mainContainer.style.width = '320px';
-   mainContainer.style.transition = 'width 0.3s ease';
-});
-
-mainContainer.addEventListener('mouseout', () => {
-   mainContainer.style.width = '50px';
-});
-
-// container unrated
-const containerUnrated = document.getElementById('container-unrated');
+// Container unrated
+const containerUnrated = document.querySelector('.container-unrated');
 containerUnrated.addEventListener('mousemove', () => {
-   containerUnrated.style.backgroundColor = '#2a2a2e';
-   containerUnrated.style.transition = 'width 0.3s ease';
    containerUnrated.style.width = 'auto';
-   containerUnrated.style.cursor = 'pointer';
+   containerUnrated.style.backgroundColor = '#2a2a2e';
+   containerUnrated.style.display = 'block';
+   containerUnrated.style.transition = 'width 0.3s ease';
 });
 
 containerUnrated.addEventListener('mouseout', () => {
@@ -23,70 +12,41 @@ containerUnrated.addEventListener('mouseout', () => {
    containerUnrated.style.transition = 'width 0.3s ease';
 });
 
-// style difficult
-const difficult = document.getElementById('difficult');
-mainContainer.addEventListener('mouseenter', () => {
-   difficult.style.color = '#8e8f8f';
-   difficult.style.transition = '0.2s ease';
-});
-
-mainContainer.addEventListener('mouseleave', () => {
-   difficult.style.color = 'transparent';
-});
-
-// home
-const home = document.getElementById('home');
-const homeText = document.getElementById('home-text');
-mainContainer.addEventListener('mousemove', () => {
-   home.style.color = '#c1c1c1';
-   home.style.transition = '0.5s ease';
-   home.style.zIndex = '1';
-
-   homeText.style.color = '#c1c1c1';
-   homeText.style.transition = '0.5s ease';
-});
-
-mainContainer.addEventListener('mouseout', () => {
-   home.style.color = 'transparent';
-   home.style.transition = '0.001s ease';
-   home.style.zIndex = '-1';
-
-   homeText.style.color = 'transparent';
-   homeText.style.transition = '0.001s ease';
-});
-
-// difficult easy [container]
-const containerEasy = document.getElementById('container-easy');
+// Container easy
+const containerEasy = document.querySelector('.container-easy');
+const difficultEasyLabel = document.querySelector('.difficult-easy-label');
 containerEasy.addEventListener('mousemove', () => {
-   containerEasy.style.backgroundColor = '#2a2a2e';
-   containerEasy.style.transition = 'width 0.3s ease';
    containerEasy.style.width = 'auto';
+   containerEasy.style.backgroundColor = '#2a2a2e';
+   containerEasy.style.display = 'block';
+   containerEasy.style.transition = 'width 0.3s ease';
+
+   difficultEasyLabel.style.color = 'blue';
 });
 
 containerEasy.addEventListener('mouseout', () => {
    containerEasy.style.backgroundColor = '#121216';
    containerEasy.style.transition = 'width 0.3s ease';
+
+   difficultEasyLabel.style.color = '';
 });
 
-// difficult easy [text]
-const difficultEasy = document.querySelector('.difficult-easy');
-const textDifficultEasy = document.querySelector('.text-difficult-easy');
-mainContainer.addEventListener('mousemove', () => {
-   difficultEasy.style.color = '#c1c1c1';
-   difficultEasy.style.transition = '0.5s ease';
-   difficultEasy.style.zIndex = '1';
+// Container normal
+const containerNormal = document.querySelector('.container-normal');
+const difficultNormalLabel = document.querySelector('.difficult-normal-label');
+containerNormal.addEventListener('mousemove', () => {
+   containerNormal.style.width = 'auto';
+   containerNormal.style.backgroundColor = '#2a2a2e';
+   containerNormal.style.display = 'block';
+   containerNormal.style.transition = 'width 0.3s ease';
 
-   textDifficultEasy.style.color = '#c1c1c1';
-   textDifficultEasy.style.transition = '0.5s ease';
-   textDifficultEasy.style.zIndex = '1';
+   difficultNormalLabel.style.color = 'green';
 });
 
-mainContainer.addEventListener('mouseout', () => {
-   difficultEasy.style.color = 'transparent';
-   difficultEasy.style.transition = '0.001s ease';
-   difficultEasy.style.zIndex = '-1';
+containerNormal.addEventListener('mouseout', () => {
+   containerNormal.style.backgroundColor = '#121216';
+   containerNormal.style.transition = 'width 0.3s ease';
 
-   textDifficultEasy.style.color = 'transparent';
-   textDifficultEasy.style.transition = '0.001s ease';
-   textDifficultEasy.style.zIndex = '-1';
+   difficultNormalLabel.style.color = '';
 });
+
