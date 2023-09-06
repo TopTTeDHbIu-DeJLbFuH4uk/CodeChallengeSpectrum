0ce66f0d633d571367f1d56c1a126337f9d3de40
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2023-09-06 13:53:46 +0700

    version 0.0.8

--- JS_files/modal_windows.js
+++ JS_files/modal_windows.js
@@ -8,3 +8,7 @@ const closeWindow = document.querySelector('.close-btn');
 closeWindow.addEventListener('click', () => {
    containerModalWindowEasyDifficult.style.display = 'none';
 });
+
+function redirectToPageEasyDifficult() {
+   window.open('http://localhost:63342/CodeChallengeSpectrum/difficult_easy.html?_ijt=an4f5pfjg86msk1udju0albvos&_ij_reload=RELOAD_ON_SAVE', '_self');
+}
